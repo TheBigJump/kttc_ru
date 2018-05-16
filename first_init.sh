7f17af39793866fdf4bb1561f745bce9c40283f4
TheBigJump <the_big_jump@mail.ru>
2018-05-16 06:34:28 -0400

    Modify first_init.sh

--- first_init.sh
+++ first_init.sh
@@ -3,10 +3,12 @@ mkdir -p /home/$USER/clan
 cp ./* /home/$USER/clan/
 cd /home/$USER/clan/
 rm -rf first_init.sh
+echo -e "\e[36m You are here: \e[0m"
 cd -
-echo -e "\e[42m You are here: \e[0m"
-pwd
-echo -e "\e[42m For start adding clans and users change directory \e[0m"
+echo -e "\e[36m For start adding clans and users, change directory: \e[0m"
 echo "cd /home/$USER/clan/"
-echo -e "\e[42m Launch start.sh with nohup \e[0m"
+echo -e "\e[36m Launch start.sh with nohup \e[0m"
 echo "nohup ./start.sh & "
+echo -e "\e[36m For checking count use: \e[0m"
+echo "./count.sh"
+
